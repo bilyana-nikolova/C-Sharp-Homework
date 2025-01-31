9595d2f9624f85736a8f8b8f9dbc17e1b9df032f
bilyana-nikolova <bilyana.as.nikolova@gmail.com>
2025-01-31 19:54:41 +0200

    Addes solution for DepositCalculator and UsdToBgn

---
+++ First-steps-in-coding/DepositCalculator/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+double depositSum=double.Parse(Console.ReadLine());
+int termOfDeposit=int.Parse(Console.ReadLine());
+double annualInterestRate=double.Parse(Console.ReadLine());
+double sum = depositSum + termOfDeposit * ((depositSum * annualInterestRate / 100) / 12);
+
+Console.WriteLine(sum);
---
+++ First-steps-in-coding/UsdToBGN/Program.cs
@@ -0,0 +1,5 @@
+﻿double usd = double.Parse(Console.ReadLine());
+
+double bng = 1.79549;
+double usdMoney = bng * usd;
+Console.WriteLine(usdMoney);
