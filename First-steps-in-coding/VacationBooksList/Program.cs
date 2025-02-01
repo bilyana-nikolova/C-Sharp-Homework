31761e950c278e4210e168bc7aee6feb5da93b20
bilyana-nikolova <bilyana.as.nikolova@gmail.com>
2025-02-01 17:51:17 +0200

    I wrote tasks Vacation Box List and Radians to Degrees

---
+++ First-steps-in-coding/VacationBooksList/Program.cs
@@ -0,0 +1,8 @@
+﻿int numOfPages = int.Parse(Console.ReadLine());
+int readedPagesPerHour  = int.Parse(Console.ReadLine());    
+int days = int.Parse(Console.ReadLine());
+
+int pegesPerDay = numOfPages / days;
+int hoursPerDay = pegesPerDay / readedPagesPerHour ;
+
+Console.WriteLine(hoursPerDay); 
