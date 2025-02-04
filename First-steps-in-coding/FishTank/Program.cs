378b3b563dc20f5a56920baa61d2c7cc4801fa8b
bilyana-nikolova <bilyana.as.nikolova@gmail.com>
2025-02-04 15:09:35 +0200

    Add solution for FishTank and Repainting

---
+++ First-steps-in-coding/FishTank/Program.cs
@@ -0,0 +1,12 @@
+﻿int lenghtSm = int.Parse(Console.ReadLine());
+int widthtSm = int.Parse(Console.ReadLine());
+int hightSm = int.Parse(Console.ReadLine());
+double percent = int.Parse(Console.ReadLine());
+
+int aqariumVolume = lenghtSm * widthtSm * hightSm;
+double aqariumLitersVolume = aqariumVolume / 1000;
+double takenSpace = aqariumLitersVolume * percent / 100;
+
+double neededLiters = aqariumLitersVolume - takenSpace;
+
+Console.WriteLine(neededLiters);
---
+++ First-steps-in-coding/Repainting/Program.cs
@@ -0,0 +1,20 @@
+﻿double plastic = 1.5;
+double paint = 14.5;
+double thinner = 5;
+
+
+double AmountOfPlastic = double.Parse(Console.ReadLine());
+double amountOfPaint = double.Parse(Console.ReadLine());
+double AmounOfthinner = double.Parse(Console.ReadLine());
+double Hours = double.Parse(Console.ReadLine());
+double plasticPrice = (AmountOfPlastic + 2) * plastic;
+double paintPrice = (amountOfPaint + (0.10 * amountOfPaint)) * paint;
+double thinnerPrice = AmounOfthinner * thinner;
+double forMaterials = plasticPrice + paintPrice + thinnerPrice + 0.40;
+double Worker = ( forMaterials * 0.30 ) * Hours;
+
+double totalAmount = forMaterials + Worker;
+Console.WriteLine(totalAmount);
+
+
+
